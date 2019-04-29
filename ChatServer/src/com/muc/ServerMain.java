e63ede099d94ce29857448c949616215c95d7694
cjpearl41 <cjpearl41@gmail.com>
2019-04-29 12:43:48 -0600

    Made it so there is multiple threads available, so there is no blocking in when using two terminals

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -15,19 +15,33 @@ public class ServerMain {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                OutputStream outputStream = clientSocket.getOutputStream();
-                for(int i=0; i<10; i++){
-                    outputStream.write(("The time is now " + new Date()+"\n").getBytes());
-                    Thread.sleep(1000);
-                }
-                outputStream.write("Hello World\nBy Caleb P April 2019\n".getBytes());
-                clientSocket.close();
+                Thread t = new Thread(){
+                    public void run(){
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
+
             }
 
         }catch(IOException e){
            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i=0; i<10; i++){
+            outputStream.write(("The time is now " + new Date()+"\n").getBytes());
+            Thread.sleep(1000);
+        }
+        outputStream.write("Hello World\nBy Caleb P April 2019\n".getBytes());
+        clientSocket.close();
     }
 }
