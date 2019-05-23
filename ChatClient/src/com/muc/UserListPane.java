7cd4dfc300c0c44d0fe1553a1f43ea260beb2ae7
cjpearl41 <cjpearl41@gmail.com>
2019-05-23 12:27:23 -0600

    Created a second GUI to msg a person with

--- ChatClient/src/com/muc/UserListPane.java
+++ ChatClient/src/com/muc/UserListPane.java
@@ -2,12 +2,16 @@ package com.muc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class UserListPane extends JPanel implements UserStatusListener {
     private final ChatClient client;
     private JList<String> userListUI;
     private DefaultListModel<String> userListModel;
+
+
 
     public UserListPane(ChatClient client) {
         this.client = client;
@@ -17,7 +21,27 @@ public class UserListPane extends JPanel implements UserStatusListener {
         userListUI = new JList<>(userListModel);
         setLayout(new BorderLayout());
         add(new JScrollPane(userListUI), BorderLayout.CENTER);
-    }
+
+        userListUI.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() > 1){
+                    String login = userListUI.getSelectedValue();
+                    MessagePane messagePane = new MessagePane(client, login);
+
+
+                    JFrame f = new JFrame("Message: " + login);
+                    f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    f.setSize(500, 500);
+                    f.getContentPane().add(messagePane, BorderLayout.CENTER);
+                    f.setVisible(true);
+
+                }
+            }
+        });
+
+
+        }
 
     public static void main(String[] args) {
         ChatClient client = new ChatClient("localhost", 8280);
